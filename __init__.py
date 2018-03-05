d2f58d5817b8940cbb6f49f6415da8e20aa1d476
Meitar Reihan <meitarr013@gmail.com>
2018-03-05 18:17:53 +0200

    added documentation for __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,13 @@
+# -*- coding: utf-8 -*-
+"""DiscordHooks - Discord WebHooks wrapper for Python
+
+A python module for creating and executing
+Discord WebHooks with emblems and more easily.
+
+TODO:
+    * Add examples
+
+"""
+
 from .hook import Hook
 from .embed import Embed, EmbedAuthor, EmbedField, EmbedFooter, EmbedImage, EmbedThumbnail, Color
