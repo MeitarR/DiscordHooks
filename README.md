bfa9578c031ba59293c429eb3ae6166b4fb79c39
Meitar Reihan <meitarr013@gmail.com>
2018-03-17 02:02:02 +0200

    added example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DiscordHooks
 A python module for easily execute discord webhooks with embeds and more
+## Examples
+```python
+from DiscordHooks import Hook, Embed, EmbedAuthor, Color
+from datetime import datetime
+
+webhook = 'your webhook url from discord'
+
+embed = Embed(title='look here', url='https://github.com/MeitarR', description="some embed text here :pencil:",
+              timestamp=datetime.now(), color=Color.Aqua, author=EmbedAuthor(name="Meitar"))
+
+Hook(hook_url=webhook, username="Meitar's webhook",
+     avatar_url='https://avatars1.githubusercontent.com/u/14138694',
+     content="Hello there! \U0001f62e", embeds=[embed]).execute()
+```
+![](https://i.snag.gy/xUHvqs.jpg)
--- __init__.py
+++ __init__.py
@@ -4,8 +4,23 @@
 A python module for creating and executing
 Discord WebHooks with emblems and more easily.
 
-TODO:
-    * Add examples
+GitHub:
+    https://github.com/MeitarR/DiscordHooks
+
+Example:
+    (for more examples look at the github page)
+
+    from DiscordHooks import Hook, Embed, EmbedAuthor, Color
+    from datetime import datetime
+
+    webhook = 'your webhook url from discord'
+
+    embed = Embed(title='look here', url='https://github.com/MeitarR', description="some embed text here :pencil:",
+                  timestamp=datetime.now(), color=Color.Aqua, author=EmbedAuthor(name="Meitar"))
+
+    Hook(hook_url=webhook, username="Meitar's webhook",
+         avatar_url='https://avatars1.githubusercontent.com/u/14138694',
+         content="Hello there! \U0001f62e", embeds=[embed]).execute()
 
 """
 
